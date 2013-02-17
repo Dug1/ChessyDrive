f8886224c5b338e17a13660cbd896c7ade886e64
Daguan Lu <daguanlu@gmail.com>
2013-02-17 11:22:59 -0800

    update to include all robot functions

--- src/com/bhrobotics/temp/ChessyBot.java
+++ src/com/bhrobotics/temp/ChessyBot.java
@@ -8,8 +8,12 @@
 package com.bhrobotics.temp;
 
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Victor;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,11 +26,19 @@ public class ChessyBot extends IterativeRobot {
     Joystick joystick;
     MotorModule left;
     MotorModule right;
+    Intake intake;
+    Shooter shooter;
+    DigitalInput valve;
+    Relay compressor;
     
     public void robotInit() {
-	joystick = new Joystick(3);
-	left = new MotorModule(1,2,1,2);
-	right = new MotorModule(9,10,3,4);
+	joystick = new Joystick(1);
+	left = new MotorModule(1,3,1,2);
+	right = new MotorModule(2,4,3,4);
+	intake = new Intake(new Victor(1,5), new Victor(1,6), new Victor(1,7));
+	shooter = new Shooter(new Victor(1,8), new Victor(1,9), new Encoder(1,2,1,3));
+	valve = new DigitalInput(1,1);
+	compressor = new Relay(1,1);
     }
 
     /**
@@ -38,7 +50,13 @@ public class ChessyBot extends IterativeRobot {
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic() {
+    public void teleopPeriodic() { 
+    	if(!valve.get()) {
+    	    compressor.set(Relay.Value.kForward);
+    	} else {
+    	    compressor.set(Relay.Value.kOff);
+    	}
+    	
 	double angle = joystick.getDirectionRadians();
 	double magnitude = 0;
 	if(joystick.getMagnitude() > 0.1) {
@@ -47,6 +65,7 @@ public class ChessyBot extends IterativeRobot {
 	double x = magnitude * Math.sin(angle);
 	double y = magnitude * Math.cos(angle);
 	
+	//gear shift
 	if(joystick.getRawButton(3)) {
 	    left.setHighSpeed();
 	    right.setHighSpeed();
@@ -55,12 +74,41 @@ public class ChessyBot extends IterativeRobot {
 	    right.setLowSpeed();
 	} else {}
 	
+	//intake hinge
+	
+	intake.setHingeSpeed((1 - joystick.getRawAxis(4))/2);
+	if(joystick.getRawButton(1)) {
+	    intake.bumpUp();
+	} else if(joystick.getRawButton(6)) {
+	    intake.bumpDown();
+	} else {
+	    intake.stop();
+	}
+	
+	//intake rollers
+	
+	if(joystick.getRawButton(9)) {
+	    intake.turnOn();
+	} else if(joystick.getRawButton(10)) {
+	    intake.flush();
+	} else {
+	    intake.turnOff();
+	}
+	
+	//shooter
+	shooter.setSpeed((1 - joystick.getRawAxis(5))/2);
+	if(joystick.getRawButton(2)) {
+	    shooter.turnOn();
+	} else {
+	    shooter.turnOff();
+	}
+	
 	left.set(y - x);
 	right.set(y + x);
 	System.out.println(debugString());
     }
     
     public String debugString() {
-	return "[left]:" + left.get() + "[right]:" + right.get();  
+	return "[left]:" + left.get() + "[right]:" + right.get() + "[shooter]:" + shooter.getSpeed()  + "[hinge]:" + intake.getHingeSpeed();  
     }
 }
---
+++ src/com/bhrobotics/temp/Intake.java
@@ -0,0 +1,52 @@
+package com.bhrobotics.temp;
+
+import edu.wpi.first.wpilibj.Victor;
+
+public class Intake {
+    public static final double MAX_SPEED = 0.5;
+    private double hingeSpeed = 0;
+    private Victor hinge;
+    private Victor rollerBottom;
+    private Victor rollerTop;
+
+    public Intake(Victor hinge, Victor rollerBottom, Victor rollerTop) {
+	this.hinge = hinge;
+	this.rollerBottom = rollerBottom;
+	this.rollerTop = rollerTop;
+    }
+
+    public void turnOn() {
+	rollerTop.set(-1.0);
+	rollerBottom.set(1.0);
+    }
+
+    public void turnOff() {
+	rollerTop.set(0.0);
+	rollerBottom.set(0.0);
+    }
+    
+    public void flush() {
+	rollerTop.set(1.0);
+	rollerBottom.set(-1.0);
+    }
+    
+    public void bumpUp() {
+	hinge.set(hingeSpeed);
+    }
+    
+    public void bumpDown() {
+	hinge.set(-hingeSpeed);
+    }
+    
+    public void stop() {
+	hinge.set(0.0);
+    }
+
+    public double getHingeSpeed() {
+	return hingeSpeed;
+    }
+
+    public void setHingeSpeed(double speed) {
+	this.hingeSpeed = speed;
+    }
+}
